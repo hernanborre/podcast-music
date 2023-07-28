0472bc0a5754518da9a59fd1b3944de2d3fa73eb
Hernán Borré <hernan.borre@kairosds.com>
2023-07-28 11:54:37 +0200

    refactor: remove unused tanstack lib and hooks

--- src/index.tsx
+++ src/index.tsx
@@ -5,19 +5,13 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { LoadingContextProvider } from "./infraestructure/ui/context/LoadingContext"
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-
 import "./index.css"
-
-const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element)
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <LoadingContextProvider value={{ isContextLoading: true, setIsContextLoading: () => {} }}>
-        <App />
-      </LoadingContextProvider>
-    </QueryClientProvider>
+    <LoadingContextProvider value={{ isContextLoading: true, setIsContextLoading: () => {} }}>
+      <App />
+    </LoadingContextProvider>
   </React.StrictMode>
 )
--- src/utils/test-utils/TestWrapperComponent.tsx
+++ src/utils/test-utils/TestWrapperComponent.tsx
@@ -1,26 +1,19 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { BrowserRouter, MemoryRouter } from "react-router-dom"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { LoadingContextProvider } from "../../infraestructure/ui/context/LoadingContext"
-
-const queryClient = new QueryClient()
 
 export const TesWithBrowserRouter = ({ children }: any) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <LoadingContextProvider value={{ isContextLoading: false, setIsContextLoading: () => {} }}>
-        <BrowserRouter>{children}</BrowserRouter>
-      </LoadingContextProvider>
-    </QueryClientProvider>
+    <LoadingContextProvider value={{ isContextLoading: false, setIsContextLoading: () => {} }}>
+      <BrowserRouter>{children}</BrowserRouter>
+    </LoadingContextProvider>
   )
 }
 
-export const TestWithMemoryRouter = ({ children } : any) => {
+export const TestWithMemoryRouter = ({ children }: any) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <LoadingContextProvider value={{ isContextLoading: false, setIsContextLoading: () => {} }}>
-        <MemoryRouter>{children}</MemoryRouter>
-      </LoadingContextProvider>
-    </QueryClientProvider>
+    <LoadingContextProvider value={{ isContextLoading: false, setIsContextLoading: () => {} }}>
+      <MemoryRouter>{children}</MemoryRouter>
+    </LoadingContextProvider>
   )
 }
